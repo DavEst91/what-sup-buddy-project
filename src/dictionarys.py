44aaeeebf23d3fb3de04e429dbde1657266f214c
DavEst91 <davidestebanmendoza@gmail.com>
2020-05-19 17:21:22 +0200

    lista_canciones

--- src/dictionarys.py
+++ src/dictionarys.py
@@ -1,1 +1,10 @@
 emotion_numbers={0:"Angry", 1:"Disgust", 2:"Fear", 3:"Happy", 4:"Sad", 5:"Surprise", 6:"Neutral"}
+playlists={
+           0:"https://open.spotify.com/playlist/6ft4ijUITtTeVC0dUCDdvH?si=uVkuP31wTl-cWeY-zU-cDw",
+           1:"https://open.spotify.com/playlist/55rCoSgFj81V4IKVNAvJBE?si=AazqKnuUS7S0LWqgpmC-sw",
+           2:"https://open.spotify.com/playlist/54flGgbTgKwBBUC3TtK0J4?si=DhGpA9s8R6K_-ANr_K24-w",
+           3:"https://open.spotify.com/playlist/37i9dQZF1DXdPec7aLTmlC?si=f4IxWMr8S0qxelOEbJmEBg",
+           4:"https://open.spotify.com/playlist/7ABD15iASBIpPP5uJ5awvq?si=Xl40pDoOTp2-JEH9FY2vxA",
+           5:"https://open.spotify.com/playlist/4o5SxWNsTNLOi9ahHeJF8A?si=kOe3kDasSayUO817ohCk8A",
+           6:"https://open.spotify.com/playlist/5vB26lbUwerJrn2WTgIndc?si=3DgQbOnFTO699AMOM7OrZg"
+           }
